19eed7283cdf6a4cc3854445bc5b5b67cac4c524
Oliver Steffmann <olli@amazon.com>
2022-09-16 12:52:34 -0400

    updated CF templates

--- analytics/consumer/glue/init_schema.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-S3_BUCKET=`aws ssm get-parameter --name "/copilot/applications/$COPILOT_APPLICATION_NAME/$COPILOT_ENVIRONMENT_NAME/public-blockchain-data-s3bucket" | jq -r '.Parameter | .Value'`
-echo "s3bucket=${S3_BUCKET}" 
-
-aws cloudformation deploy --template-file=schema.yaml --parameter-overrides "s3bucket=${S3_BUCKET}" --stack-name=public-blockchain-data-schema --capabilities=CAPABILITY_IAM
